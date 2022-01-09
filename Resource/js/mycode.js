2718456dd3d54abe3653920eba7f1c1ec6f8a732
DatVt17 <vutiendat179@gmail.com>
2022-01-09 14:59:40 +0700

    test
    ok

---
+++ Resource/js/mycode.js
@@ -0,0 +1,9 @@
+
+function openNav(){
+    document.getElementsByClassName('sidenav').style.display = "block";
+    document.getElementsByClassName('idenav').style.innerHTML.width = "100";
+}
+function closeNav(){
+    document.getElementById('closebtn').style.display = "none";
+    
+}
